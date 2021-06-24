bd97dc12e8b8a75f7a1d80ccb44daeb4da810d40
grcgs2212 <b07901039@ntu.edu.tw>
2021-06-24 12:13:58 +0800

    aug fix

--- data/wider_face.py
+++ data/wider_face.py
@@ -5,9 +5,10 @@ import torch
 import torch.utils.data as data
 import cv2
 import numpy as np
+import imgaug.augmenters as iaa
 
 class WiderFaceDetection(data.Dataset):
-    def __init__(self, txt_path, preproc=None):
+    def __init__(self, txt_path, preproc=None, aug=False):
         self.preproc = preproc
         self.imgs_path = []
         self.words = []
@@ -33,6 +34,37 @@ class WiderFaceDetection(data.Dataset):
                 labels.append(label)
 
         self.words.append(labels)
+        self.seq = iaa.Sequential([
+                            iaa.SomeOf((0, 5),
+                            [
+                                iaa.OneOf([
+                                    iaa.GaussianBlur((0, 3.0)), # blur images with a sigma between 0 and 3.0
+                                    iaa.AverageBlur(k=(2, 7)), # blur image using local means with kernel sizes between 2 and 7
+                                    iaa.MedianBlur(k=(3, 11)), # blur image using local medians with kernel sizes between 2 and 7
+                                ]),
+                                iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)), # sharpen images
+                                iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+                                # search either for all edges or for directed edges,
+                                # blend the result with the original image using a blobby mask
+                                iaa.SimplexNoiseAlpha(iaa.OneOf([
+                                    iaa.EdgeDetect(alpha=(0.5, 1.0)),
+                                    iaa.DirectedEdgeDetect(alpha=(0.5, 1.0), direction=(0.0, 1.0)),
+                                ])),
+                                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5), # add gaussian noise to images
+                                iaa.OneOf([
+                                    iaa.Dropout((0.01, 0.1), per_channel=0.5), # randomly remove up to 10% of the pixels
+                                    iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                                ]),
+                                iaa.Invert(0.05, per_channel=True), # invert color channels
+                                iaa.Add((-10, 10), per_channel=0.5), # change brightness of images (by -10 to 10 of original value)
+                                iaa.AddToHueAndSaturation((-20, 20)), # change hue and saturation
+                                # either change the brightness of the whole image (sometimes
+                                # per channel) or change the brightness of subareas
+                                iaa.Grayscale(alpha=(0.0, 1.0)),
+                                ],
+                                random_order=True
+                                )
+                            ])
 
     def __len__(self):
         return len(self.imgs_path)
@@ -72,6 +104,8 @@ class WiderFaceDetection(data.Dataset):
             annotations = np.append(annotations, annotation, axis=0)
         target = np.array(annotations)
         if self.preproc is not None:
+            if self.aug:
+                img = self.seq(img)
             img, target = self.preproc(img, target)
 
         return torch.from_numpy(img), target
